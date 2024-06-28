60258ff01835a3f0aea2ccd73a60f0100a4ae52c
Clayton Sadler <csadler@paychex.com>
2024-06-28 15:46:33 -0400

    Working through possible structure

---
+++ CallEngine/HttpCall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerInventory.CallEngine
+{
+    //This will eventually become the base class for our factory. We are going to use it to set up our independant call framework for now. 
+    internal class HttpCall
+    {
+       public async Task InitialCall(HttpCredentials creds, string path, string queryParams)
+        {
+
+        }
+    }
+}
